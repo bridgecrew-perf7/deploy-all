dc3efdc706045725e6f384f6be5ec6b6cb8d2c90
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-05-24 20:45:03 +0300

    dd

--- src/main/java/IP/DemoApplication.java
+++ src/main/java/IP/DemoApplication.java
@@ -1,15 +1,11 @@
 package IP;
 
-import IP.entity.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import IP.repository.*;
 import IP.root.ManagerFactorySingleton;
-import IP.service.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -19,13 +15,8 @@ public class DemoApplication {
 
     public static void main(String[] args) throws Exception {
 
-
         ManagerFactorySingleton.setName("heroku_ab221cef7d5f484");
-
-
         SpringApplication.run(DemoApplication.class, args);
-
-
     }
 
     @Bean
