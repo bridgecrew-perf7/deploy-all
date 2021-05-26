9800de20919c5b2d467f72bed2b1529df874f65f
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-05-26 20:53:11 +0300

    i wannna die

--- src/main/java/IP/DemoApplication.java
+++ src/main/java/IP/DemoApplication.java
@@ -6,10 +6,12 @@ import IP.root.ManagerFactorySingleton;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
+@EnableWebMvc
 @Configuration
 public class DemoApplication {
 
